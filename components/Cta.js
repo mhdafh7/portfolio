08f9fa6c996497ab4c59fab2c64d36e95f200bd1
Mhdafh <mhdafh@tuta.io>
2022-09-25 08:36:14 +0530

    UI corrections

--- components/Cta.js
+++ components/Cta.js
@@ -1,7 +1,14 @@
-import styles from '../styles/Cta.module.scss'
-const Cta = () => {
-  return (
-    <div className={styles.container}></div>
-  )
-}
+import styles from "../styles/Cta.module.scss"
+import { ArrowRightShort } from "react-bootstrap-icons"
+const Cta = ({ classname,text }) => (
+    <div className={`${styles.container} ${styles[classname]}`}>
+        <div className={styles.wrapper}>
+            <h3>
+                {/*eslint-disable-next-line react/no-unescaped-entities*/}
+                {text}
+            </h3>
+            <ArrowRightShort className={styles.icon} />
+        </div>
+    </div>
+)
 export default Cta
