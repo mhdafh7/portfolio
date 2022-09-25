144b79172becec73e9c85711411e67e25c5128f3
Mhdafh <mhdafh@tuta.io>
2022-09-25 08:33:50 +0530

    build error apos

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,26 @@
 import styles from "../styles/Hero.module.scss"
+import Cta from "./Cta"
+import Navbar from './Navbar'
+
 const Hero = () => {
     return (
         <div className={styles.container}>
-            <p className={styles.description}>Hey! I'm Muhammed Aflah.</p>
-            <h1 className={styles.title}>Visual Designer & Frontend dev.</h1>
+            <div className={styles.textAndNav}>
+                <p className={styles.description}>Hey! I&apos;m Aflah.</p>
+                <h1 className={styles.title}>
+                    Full stack
+                    <br />
+                    developer.
+                </h1>
+                <p className={styles.description}>
+                    Browser is my canvas. Coding is my paint.
+                </p>
+                <Navbar />
+            </div>
+            <div className={styles.ctaContainer}>
+                <Cta classname="fill" text="Let's get in touch "/>
+                <Cta classname="outline" text="Resume "/>
+            </div>
         </div>
     )
 }
