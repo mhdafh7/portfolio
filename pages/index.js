cd864a13c5bec1be3e15d701c71a43fbd9dd3209
Mhdafh <mhdafh@tuta.io>
2022-11-02 21:43:45 +0530

    Added Footer

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from "../components/Header"
 import Hero from "../components/Hero"
 import About from "../components/About"
 import Projects from "../components/Projects"
+import Footer from "../components/Footer"
 
 export default function Home() {
     return (
@@ -29,6 +30,7 @@ export default function Home() {
                 <About />
                 <Projects />
             </main>
+            <Footer />
         </div>
     )
 }
