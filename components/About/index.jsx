0ce12df582653b534707d33c273e9af82fa5a7c6
mhdafh7 <muhammed.aflah@leegality.com>
2022-12-10 10:03:00 +0530

    layout: updated layout and styles

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -1,27 +1,29 @@
-import styles from "./About.module.scss"
+import styles from './About.module.scss';
 
 const About = () => {
     return (
-        <section className={styles.wrapper}>
-            <div className={styles.container}>
+        <section className={styles.container}>
+            <div className={styles.frameItem}>
                 <div className={styles.title}>
                     <h3>
-                        Hey, I am Muhammed Aflah<span>.</span>
+                        <span>I am</span> <br /> Muhammed Aflah<span>.</span>
                     </h3>
                 </div>
-                <div className={styles.bio}>
-                    <p>
-                        Love making pixel perfect interfaces, and write clean
-                        server side code. I am passionate about web technologies
-                        and the creative experiences they create.
-                    </p>
-                    <p>
-                        Well if you are not interested in those things, follow
-                        me on twitter and we can fight over football opinions :)
-                    </p>
-                </div>
+            </div>
+            <div className={styles.frameItem}>
+                <p className={styles.bio}>
+                    Love making pixel perfect interfaces, and write clean server side
+                    code. I am passionate about web technologies and the creative
+                    experiences they create.
+                </p>
+            </div>
+            <div className={styles.frameItem}>
+                <p className={styles.bio}>
+                    Well if you are not interested in those things, follow me on twitter
+                    and we can fight over football opinions :)
+                </p>
             </div>
         </section>
-    )
-}
-export default About
+    );
+};
+export default About;
