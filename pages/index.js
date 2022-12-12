cdf2c6b6a36c84b173576b4209f885c3f33e500c
mhdafh7 <muhammed.aflah@leegality.com>
2022-12-12 08:29:59 +0530

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Head from "next/head"
-import styles from "../styles/Home.module.scss"
-import Header from "../components/Header"
-import Hero from "../components/Hero"
-import About from "../components/About"
-import Projects from "../components/Projects"
-import Footer from "../components/Footer"
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Footer from '../components/Footer';
 
 export default function Home() {
+
     return (
         <div className={styles.container}>
             <Head>
@@ -32,5 +33,5 @@ export default function Home() {
             </main>
             <Footer />
         </div>
-    )
+    );
 }
