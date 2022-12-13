a445af49cf4c449e6feb456ca29c901435268e2a
mhdafh7 <muhammed.aflah@leegality.com>
2022-12-13 15:16:58 +0530

    style: updated colors

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import Projects from '../components/Projects';
 import Footer from '../components/Footer';
 
 export default function Home() {
-
     return (
         <div className={styles.container}>
             <Head>
