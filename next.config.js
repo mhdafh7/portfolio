64d17634f632b07abf8964c68315b1b01f206d5a
mhdafh7 <muhammed.aflah@leegality.com>
2022-12-20 08:16:34 +0530

    turned off strict mode for locomotive scroll

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     swcMinify: true,
+    images: {
+        unoptimized: true,
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/i,
             issuer: /\.[jt]sx?$/,
-            use: ["@svgr/webpack"],
-        })
+            use: ['@svgr/webpack'],
+        });
 
-        return config
+        return config;
     },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
